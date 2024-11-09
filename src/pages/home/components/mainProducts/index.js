8320a53e89537e0f6ff06e0b27162c588d121cb3
Eugenius 1st <eugenius1st@gmail.com>
2024-11-09 19:30:07 +0900

    feat: 깜빡거리는 효과 추가

--- src/pages/home/components/mainProducts/index.js
+++ src/pages/home/components/mainProducts/index.js
@@ -7,7 +7,7 @@ const MainProducr = () => {
   const products = [
     {
       title: "부사사과",
-      state: "NEW",
+      state: "HOT",
       link: "https://smartstore.naver.com/goodmoningfarm/products/5816714650?nl-query=%EA%B5%BF%EB%AA%A8%EB%8B%9D%20%EB%86%8D%EC%9E%A5&NaPm=ci%3D62909c08b65d425d9a1e7ea2a1cb996b%7Cct%3Dm39zk17k%7Ctr%3Dnslsl%7Csn%3D1787656%7Chk%3D4f019efc509a6354f8be7c2957c6da150f1859cf",
     },
     {
@@ -73,13 +73,13 @@ const MainProducr = () => {
               <span
                 className={` top-0 left-0 text-white z-[1] text-[8px] px-[5px] pt-[2px] mr-[4px] rounded-[5px]
                 ${
-                  el.state === "NEW"
-                    ? "bg-red-600"
+                  el.state === "HOT"
+                    ? "bg-red-600 animate-blink"
                     : el.state === "SOLD OUT"
                     ? "bg-gray-600"
                     : el.state === "BEST"
                     ? "bg-yellow-500"
-                    : "bg-gray-600"
+                    : "bg-yellow-500"
                 }
                 `}
               >
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       colors: {
         "brixa-brown": "#5D2D00", // 새로운 색상 추가
       },
+      animation: {
+        blink: "blink 0.5s ease-in-out infinite", // 1초 간격으로 깜빡임
+      },
+      keyframes: {
+        blink: {
+          "0%, 100%": { opacity: 1 }, // 완전히 보임
+          "50%": { opacity: 0 }, // 투명해짐
+        },
+      },
     },
   },
   plugins: [],
